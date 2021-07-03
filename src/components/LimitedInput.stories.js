97e3db7822007afe04abacd214ccd3d4d1f815de
Chris Joyce <chris@joyce.id.au>
2021-07-04 08:38:43 +1000

    Storybook Actions Addon

--- src/components/LimitedInput.stories.js
+++ src/components/LimitedInput.stories.js
@@ -1,5 +1,7 @@
 import LimitedInput from "./LimitedInput";
 import { withKnobs, number } from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
 
 export default {
   title: "Forms/LimitedInput",
@@ -12,7 +14,25 @@ export const SimpleStory = () => ({
   },
 });
 
-SimpleStory.story = { name: "Simple Story (bis)" };
+SimpleStory.story = {
+  name: "Simple Story (bis)",
+};
+
+export const StoryForActions = () => ({
+  render() {
+    return <LimitedInput onInput={action("input")} />;
+  },
+});
+
+export const StoryForLinks = () => ({
+  render() {
+    return (
+      <button onClick={linkTo("Forms/LimitedInput", "Story For Actions")}>
+        Link
+      </button>
+    );
+  },
+});
 
 export const VariableLimit = () => ({
   props: {
